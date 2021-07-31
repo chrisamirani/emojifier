55668a86c2c727ed3632d59303eb33f770f18bc8
chrisamirani <chrisamirani@yahoo.com>
2021-07-31 14:12:02 -0700

    apply emojis based on expression

--- emojifier.js
+++ emojifier.js
@@ -1,19 +1,29 @@
 const MODEL_URL = "/models"
 const body = document.querySelector("body")
-async function emojify(yourImage){
-    let top,left,height = 0
-    let emojis = ["emoji"]
+async function emojify(imageID) {
+    let img = document.querySelector(imageID)
+    let top, left, height = 0
+    let emojis = ['neutral', 'happy', 'sad', 'angry', 'fearful', 'disgusted', 'surprised']
     await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
     await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL)
+    await faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL)
 
-    
-    const detections = await faceapi.detectAllFaces(yourImage)
-    
-    for(face in detections){
-        top = detections[face]._box._y
-        left = detections[face]._box._x
-        height = detections[face]._box._height
-        let emoji = `<img src="./emojis/${emojis}.png" style="position: absolute;height: ${height}px;left: ${left}px;top: ${top}px;">`
+
+    const detections = await faceapi.detectAllFaces(img).withFaceExpressions()
+
+    for (face in detections) {
+        top = detections[face].detection._box._y
+        left = detections[face].detection._box._x
+        height = detections[face].detection._box._height
+        let emojiPic = "happy"
+
+
+        for (expr in emojis) {
+            if (detections[face].expressions[emojis[expr]] > 0.8) {
+                emojiPic = emojis[expr]
+            }
+        }
+        let emoji = `<img src="./emojis/${emojiPic}.png" style="position: absolute;height: ${height}px;left: ${left}px;top: ${top}px;">`
         body.innerHTML += emoji
     }
 }
