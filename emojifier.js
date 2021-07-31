ebc26a21a3b72a9a4d180f08914532601eb0aeff
chrisamirani <chrisamirani@yahoo.com>
2021-07-31 13:04:53 -0700

    added emojis list

--- emojifier.js
+++ emojifier.js
@@ -2,6 +2,7 @@ const MODEL_URL = "/models"
 const body = document.querySelector("body")
 async function emojify(yourImage){
     let top,left,height = 0
+    let emojis = ["emoji"]
     await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
     await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL)
 
@@ -12,7 +13,7 @@ async function emojify(yourImage){
         top = detections[face]._box._y
         left = detections[face]._box._x
         height = detections[face]._box._height
-        let emoji = `<img src="./emoji.png" style="position: absolute;height: ${height}px;left: ${left}px;top: ${top}px;">`
+        let emoji = `<img src="./emojis/${emojis}.png" style="position: absolute;height: ${height}px;left: ${left}px;top: ${top}px;">`
         body.innerHTML += emoji
     }
 }
