af6a11bd9f0aa8dd3c3019dd02cad0bbd20fccc7
chrisamirani <chrisamirani@yahoo.com>
2021-07-31 12:55:42 -0700

    first emojifier commit

---
+++ emojifier.js
@@ -0,0 +1,18 @@
+const MODEL_URL = "/models"
+const body = document.querySelector("body")
+async function emojify(yourImage){
+    let top,left,height = 0
+    await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
+    await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL)
+
+    
+    const detections = await faceapi.detectAllFaces(yourImage)
+    
+    for(face in detections){
+        top = detections[face]._box._y
+        left = detections[face]._box._x
+        height = detections[face]._box._height
+        let emoji = `<img src="./emoji.png" style="position: absolute;height: ${height}px;left: ${left}px;top: ${top}px;">`
+        body.innerHTML += emoji
+    }
+}
